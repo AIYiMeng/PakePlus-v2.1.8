3c92ad93d4c3ab75c64aa920fbbacd0c23ed033b
乂忆 <64470752+AIYiMeng@users.noreply.github.com>
2026-03-04 16:38:25 +0800

    update src file from pakeplus

---
+++ src/assets/user-0prxlamg.js
@@ -0,0 +1,1 @@
+import{a as s}from"./index-SK50eeeU.js";const t=r=>s.post("/api/v1/users/login",r),o=r=>s.post("/api/v1/users/register",r),i=()=>s.get("/api/v1/users/info");export{i as g,t as l,o as r};
